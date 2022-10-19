400ad536b31693ad6028ace9b2b006671f885770
Prokopevs <prokopev_savva@mail.ru>
2022-10-19 19:42:38 +0300

    fizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,34 @@
+function foo (number) {
+    if(number%5 === 0) {
+        if(number%3 === 0) {
+            console.log("FizzBuzz")
+        }
+    } else if(number%5 === 0) {
+        console.log("Buzz")
+    } else if(number%3 === 0) {
+        console.log("Fizz")
+    } else {
+        console.log(number)
+    }
+}
+
+for (let i=0; i<100; i++) {
+    foo(i)
+}
+
+// for (let i=0; i<100; i++) {
+//     if(i%5 === 0 || i%3 === 0) {
+//         if(i%5 === 0 && i%3 === 0) {
+//             console.log("FizzBuzz");
+//             continue;
+//         }
+//         if(i%5 === 0) {
+//             console.log("Buzz")
+//         }
+//         if(i%3 === 0) {
+//             console.log("Fizz")
+//         }
+//     } else {
+//         console.log(i)
+//     }
+// }
